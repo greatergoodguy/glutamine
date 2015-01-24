6035e34f3241335a8389f290f64b2b97beec7682
Tom Yu <tom.yu@burstingbrains.com>
2015-01-24 17:33:23 -0600

    Stable commit

---
+++ Assets/Glutamine/Script/AI/AIBigBunny.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIBigBunny : MonoBehaviour {
+
+	public static readonly string TAG = typeof(AIBigBunny).Name;
+
+	AudioSource audio;
+
+	void Awake() {
+		audio = GetComponent<AudioSource>();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		UtilLogger.Log(TAG, "OnTriggerEnter()");
+		if(other.tag == "Player") {
+			audio.Play();
+		}
+	}
+}
