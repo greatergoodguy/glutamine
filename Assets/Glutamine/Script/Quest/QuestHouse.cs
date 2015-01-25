b3f6e8683eeaf0bba66bb4e1e10aad472490f417
Tom Yu <tom.yu@burstingbrains.com>
2015-01-25 13:50:16 -0600

    Stable commit

---
+++ Assets/Glutamine/Script/Quest/QuestHouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestHouse : MonoBehaviour {
+
+	GameObject goBoy;
+	GameObject goHouse;
+
+	void Start() {
+		goBoy = transform.FindChild("boywithHouse").gameObject;
+		goHouse = transform.FindChild("HouseForGlutamine").gameObject;
+	}
+	
+	public void Activate() {
+		goHouse.rigidbody.AddRelativeTorque(50000 * Vector3.forward);
+
+		GeneSpeech geneSpeech = goBoy.GetComponent<GeneSpeech>();
+		geneSpeech.text = "WHY DID YOU DO THAT!!!";
+	}
+	
+}
+
